9b2b8b9d008e2ddacddcc396203d19e9bcb08679
chrisho251 <hoduyhoang250196@gmail.com>
2022-09-11 17:59:41 +0700

    Update Font

---
+++ G4Rum/Models/FontManager.swift
@@ -0,0 +1,16 @@
+//
+//  FontManager.swift
+//  G4Rum
+//
+//  Created by Hoang, Ho Duy on 11/09/2022.
+//
+
+import SwiftUI
+
+struct FontManager {
+    struct Playfair {
+        static let bold = "PlayfairDisplay-Bold"
+        static let regular = "PlayfairDisplay-regular"
+    }
+}
+
