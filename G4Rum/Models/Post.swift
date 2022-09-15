7ce09555c68f13d15078930ba8f34247dfb1668e
Quoc Thang <53080086+s3713564@users.noreply.github.com>
2022-09-15 18:20:27 +0700

    Update Post.swift

--- G4Rum/Models/Post.swift
+++ G4Rum/Models/Post.swift
@@ -15,3 +15,12 @@ struct Post: Identifiable, Codable {
     var createdDate: String
     var totalComment: Int
 }
+
+var posts = [
+    Post(id: "1", title: "GG Well Play", content: "Troi hom nay nhieu may cuc dat ban tay minh ngay nguc, mua thu mang giac mo quay ve an nguyen ven nhu hom nay la bay theo gio son sao, chon xua anh ho, doan duong ngay nao hai tta tung don dua con van vuong phai mo giau yeu6 thuong trong van tho chung1 ta la ang may ngang troi tvoii vang ngang 1ua chung ta ", username: "Juan Chavez", createdDate: "Apr 29",  totalComment: 1),
+    Post(id: "2", title: "GG Well Play", content: "hg", username: "Jua", createdDate: "Apr 25", totalComment: 1),
+    Post(id: "3", title: "GG Well Play", content: "hello hi", username: "Ju", createdDate: "A 29", totalComment: 1),
+    Post(id: "4", title: "GG Well Play", content: "hello hi", username: "Tong", createdDate: "Apr 29", totalComment: 1),
+    Post(id: "5", title: "GG Well Play", content: "hello hi", username: "Nguyen", createdDate: "Apr 29", totalComment: 1),
+    Post(id: "6", title: "GG Well Play", content: "hello hi", username: "Nhi", createdDate: "Apr 29", totalComment: 1),
+]
